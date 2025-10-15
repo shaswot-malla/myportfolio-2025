895bcea579c7650671a0dd36ea2933f3e7d5f2da
Shaswot <smallathakuri@sfc.edu>
2025-10-15 11:50:37 -0400

    Navbar Completed - Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero";
+
+const App = () => {
+  return(
+    <div className='container  mx-auto max-w-7xl'> 
+      <Navbar />
+      <Hero />
+    
+     </div>
+  )
+}
+
+export default App;
---
+++ src/components/HeroText.jsx
@@ -0,0 +1,24 @@
+
+
+const HeroText = () => {
+  return (
+    <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+        {/* DesktopView */}
+        <div className="flex-col hidden md:flex c-space">
+            <h1 className="text-4xl font-medium">Greetings! Im Shaswot </h1>
+            <br></br>
+            <div className="flex flex-col items-start">
+                <p className="text-3xl font-medium text-neutral-300">An artist and a designer <br/> 
+                who rock n rolls and is dedicated to crafting awesome designs</p>
+            {/* <div>
+               Get flip word from aceternity 
+            </div> */}
+
+            </div>
+        </div>
+        {/* { MobileView } */}
+    </div>
+  )
+}
+
+export default HeroText
