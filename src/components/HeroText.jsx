ee6a7dbde2c722e4d69dc44ae92aab39827b1826
Shaswot <smallathakuri@sfc.edu>
2025-10-17 14:20:01 -0400

    Motion added in h1 and p tags in HeroText.jsx

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -1,27 +1,79 @@
 import { FlipWords } from "./FlipWords"
 
+import { motion } from "motion/react";
 
 const HeroText = () => {
+    const words=[ "Modern", "Appealing", "Fun", "Scalable"];
+    const variants = {
+        hidden:{opacity:0, x:-50},
+        visible:{opacity:1, x:0}
+    };
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
         {/* DesktopView */}
         <div className="flex-col hidden md:flex c-space">
-            <h1 className="text-4xl font-medium">Greetings! Im Shaswot </h1>
+            <motion.h1 className="text-4xl font-medium" 
+            variants={variants}
+            initial="hidden"
+            animate ="visible"
+            transition={{delay:1}} > Greetings! Im Shaswot </motion.h1>
             <br></br>
             <div className="flex flex-col items-start">
-                <p className="text-3xl font-medium text-neutral-300">An artist and a designer <br/> 
-                who rock n rolls and is dedicated to crafting awesome designs</p>
-            <div>
-               <FlipWords words={["Secure", "Modern", "Fun", "Scalable"]} className={"font-black text-white text-8xl"}/>
-            </div>
-            <p className="text-2xl font-medium text-neutral-300">
-                Web Design and Solutions 
-            </p>
+                <motion.p className="text-3xl font-medium text-neutral-300"
+                variants={variants}
+                initial="hidden"
+                animate ="visible"
+                transition={{delay:1.2}}>An artist and a designer <br/> 
+                who rock n rolls and is dedicated to crafting awesome designs</motion.p>
+            <motion.div
+            variants={variants}
+            initial="hidden"
+            animate ="visible"
+            transition={{delay:1.5}}>
+               <FlipWords words={words} className={"font-black text-white text-8xl"}/>
+            </motion.div>
+            <motion.p className="text-2xl font-medium text-neutral-300" 
+            variants={variants}
+            initial="hidden"
+            animate ="visible"
+            transition={{delay:1.8}}>
+                 Design and Solutions 
+            </motion.p>
             </div>
         </div>
         {/* { MobileView } */}
+        <div className="flex- flex-col space-y-6 md:hidden">
+        <motion.p className="text-3xl font-medium"
+        variants={variants}
+        initial="hidden"
+        animate ="visible"
+        transition={{delay:1}}>Hi! I'm Shas </motion.p>
+        <motion.p className="text-5xl font-black text-neutral-300"
+        variants={variants}
+        initial="hidden"
+        animate ="visible"
+        transition={{delay:1.2}}>Building </motion.p>
+        <motion.div
+        variants={variants}
+        initial="hidden"
+        animate ="visible"
+        transition={{delay:1.5}}>
+            <FlipWords words={[ "Modern", "Appealing", "Fun", "Scalable"]} className={"font-bold text-white text-7xl"}/>
+        </motion.div>
+
+        <motion.p className="text-4xl font-black text-neutral-300"
+        variants={variants}
+        initial="hidden"
+        animate ="visible"
+        transition={{delay:1.8}}>Designs for Everything</motion.p>
+        </div>
     </div>
   )
 }
 
 export default HeroText
+
+
+// Git add . 
+// Git commit -m "bla bla"
+// Git push origin main
