deb670e00c99989a9deaf5a04013ee5c8809702f
Shaswot <smallathakuri@sfc.edu>
2025-11-03 13:33:27 -0500

    Astronaut added

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,11 +3,15 @@
 import { Canvas } from "@react-three/fiber"
 import HeroText from "../components/HeroText"
 import ParallaxBackground from "../components/ParallaxBackground"
+import { OrbitControls } from "@react-three/drei"
+import { Astronaut } from "../components/Astronaut"
+import { useMediaQuery } from "react-responsive"
 
 const Hero = () => {
+  const isMobile = useMediaQuery({maxWidth: 853});
   return (
     <section className="flex items-start justify-center md:items-start
-    md: justify-start min-h-screen overflow-hidden c-space">
+    md:justify-start min-h-screen overflow-hidden c-space">
 
         <HeroText/>
         <ParallaxBackground/>
@@ -15,8 +19,12 @@ const Hero = () => {
         className="absolute inset-0" 
         style={{width: "100vw", height: "100vh"}}
         >
-          <Canvas>
-            
+          <Canvas camera={{position:[0,1,3]}}>
+            <Astronaut 
+            scale={isMobile && 0.3} 
+            position={isMobile && [0,-1.5,0]}/>
+            <OrbitControls/> 
+            {/* oribitControls not working and letting me drag the 3d Model */}
           </Canvas>
         </figure>
         </section>
