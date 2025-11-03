8c2de1ef1d4d0c1607c1e30c90ec7d773ad3f426
Shaswot <smallathakuri@sfc.edu>
2025-11-03 13:56:54 -0500

    useFrame, useSpring, and useEffect added to the 3d model

--- src/components/Astronaut.jsx
+++ src/components/Astronaut.jsx
@@ -9,6 +9,8 @@ Title: Tenhun Falling spaceman (FanArt)
 
 import React, { useEffect, useRef } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
+import { useMotionValue, useSpring } from 'motion/react'
+import { useFrame } from '@react-three/fiber'
 
 export function Astronaut(props) {
   const group = useRef()
@@ -19,6 +21,15 @@ export function Astronaut(props) {
         actions[animations[0].name]?.play();
     }
   }, [actions, animations]);
+
+  const yPosition = useMotionValue(5);
+  const ySpring = useSpring(yPosition, { damping: 30});
+  useEffect(()=>{
+    ySpring.set(-1);
+  }, [ySpring]);
+  useFrame(()=>{
+    group.current.position.y = ySpring.get();
+  });
   return (
     <group ref={group} {...props} dispose={null}
     rotation={[-Math.PI / 2, -0.2, 2.2]} 
